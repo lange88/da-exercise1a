b2da3239119720a87c5b5bd2a0630aab4ec77a94
Jeroen Vrijenhoef <jeroen@vrijenhoef.net>
2017-11-16 10:11:32 +0100

    added basic framework

---
+++ src/main/java/DA_BSS_main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by jeroen on 11/16/17.
+ */
+public class DA_BSS_main {
+    public static void main(String[] args) {
+        System.out.println("jaa");
+    }
+}
---
+++ src/main/java/interfaces/DA_BSS_RMI.java
@@ -0,0 +1,10 @@
+package interfaces;
+
+import java.rmi.Remote;
+
+/**
+ * Created by jeroen on 11/16/17.
+ */
+public interface DA_BSS_RMI extends Remote{
+    public void method1() throws java.rmi.RemoteException;
+}
